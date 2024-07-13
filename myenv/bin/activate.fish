faad2765642fba5723fcabf93bdf2a9ad11564f3
Ahimsa Das <ahimsadas@Ahimsas-MacBook-Pro.local>
2024-07-13 21:50:37 +0530

    minor changes

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ahimsadas/Documents/Code/monthly_growth_calculator/myenv"
+set -gx VIRTUAL_ENV "/Users/ahimsadas/Documents/Code/monthly_growth_calculator/monthly_growth_calculator/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
